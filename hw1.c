b75a3cb25465e7eced52d7d17842b468b743601f
Andrew Thomas <andrewthomas@andrews-mbp.ssw.umb.edu>
2018-09-25 14:50:04 -0400

    Removed comment - I'm at a loss for what isn't working on the autograder

--- hw1.c
+++ hw1.c
@@ -1,17 +1,3 @@
-/*
-Write a program: RemoveEmptyLines.c
-- Input: a text file
-- Output: a text file exactly same as the input file except that all the empty lines
-        are removed
-Requirement: the only input/output functions you can use are getchar() and
-putchar(). If you want to use any other function, you have to implement it by
-yourself.
-To run your program at your local computer, type the following at the command line
-prompt:
-RemoveEmptyLines < input_file > output_file where you provide input_file as the name of
-the input file and output_file as the name of the output file.
-*/
-
 #include <stdio.h>
 
 int main(){
