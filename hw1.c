06d3de120dfbdaefae16c1d5e4738e140f7d855a
Andrew Thomas <andrewthomas@andrews-mbp.ssw.umb.edu>
2018-10-02 18:08:33 -0400

    Homework One Passes Test

--- hw1.c
+++ hw1.c
@@ -1,22 +1,79 @@
 #include <stdio.h>
-int main(){
-    char c;
-    int newline = 0;
-    while(1){
+
+char getLine(char* line){
+    int i = 0;
+    char c = ' ';
+
+    while (1){
         c = getchar();
-        if (c == EOF){
+        if (c != EOF && c != '\n' && c != '\r'){
+            line[i] = c;
+            i++;
+        }
+         else if (c == EOF){
+            line[i] = EOF;
+            i++;
+            line[i] = '\0';
             break;
         }
-        if (c == '\n' && newline == 0){
-            newline = 1;
-            putchar(c);
-            continue;
+        else{
+            line[i] = c;
+            i++;
+            line[i] = '\0';
+            break;
         }
-        while (newline == 1 && c == '\n'){
-            c = getchar();
-        }
-        putchar(c);
-        newline = 0;
     }
+    return *line;
+}
+
+int hasChar(char* s, int length){
+    int i;
+    for (i = 0; i < length; i++){
+      
+        if (s[i] != ' ' && s[i] != '\t' && s[i] != '\n' && s[i] != '\r' && s[i] != EOF){
+            return 1;
+        }
+      
+        if (s[i] == '\n' || s[i] == '\r' || s[i] == '\0' || s[i] == EOF){
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int hasEOF(char* s, int length){
+    int i;
+    for (i = 0; i < length; i++){
+        if (s[i] == EOF){
+            return 1;
+        }
+        if (s[i] == '\n' || s[i] == '\r' || s[i] == '\0'){
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    const int SIZE = 1000;
+    int i;
+
+    while (1){
+        char line[SIZE];
+
+        getLine(line);
+
+        if (hasChar(line, SIZE)){
+            i = 0;
+            while (line[i] != EOF && line[i] != '\0'){
+                putchar(line[i]);
+                i++;
+            }
+        }
+        if (hasEOF(line, SIZE)){
+            break;
+        }
+    }
+
     return 0;
 }
---
+++ hw2.c
@@ -0,0 +1,120 @@
+//
+// hw.2
+//
+//
+// Created by Andrew Thomas on 10/1/18
+//
+//
+
+#include <stdio.h>
+<<<<<<< HEAD
+<<<<<<< HEAD
+#include <stdlib.h>
+=======
+>>>>>>> 4f43b12177ce0c47a69d83d8e8d10e2beee07d9a
+=======
+>>>>>>> 4f43b12177ce0c47a69d83d8e8d10e2beee07d9a
+
+#define MAXLEN 10000
+
+
+void splitString(char s[], char d, char** word){
+    // implement the splitting algorithm
+    // now reading string s to extract words
+    int n = 0; // order of the word in the string
+    int i = 0;     // position in string s
+    int j = 0; // position in word
+    char c;
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+    word = malloc(MAXLEN * MAXLEN * sizeof(char));
+
+=======
+>>>>>>> 4f43b12177ce0c47a69d83d8e8d10e2beee07d9a
+=======
+>>>>>>> 4f43b12177ce0c47a69d83d8e8d10e2beee07d9a
+    // check if s is empty
+
+    if (s[0] == 0){
+        // s is empty
+        word[0][0] = 0;
+
+    }
+    while(1){
+        c = s[i];
+            
+
+        if (c == d){
+            // see the delimiter char
+            word[n][j] = 0; // null character
+            j = 0;
+            n++; // ready for next word
+        }
+        else{
+            word[n][j] = c;
+            j++;
+        }
+        i++;
+
+        // is this the last character of string s
+        if (s[i] == 0){
+            // check if previous character is delimiter or not
+            if (c == d){
+                // previous character is the same as delimiter
+                word[n][0] = 0; // empty word
+            }
+            else {
+                break;
+            }
+        }
+    
+    }
+
+    // so far we have (n+1) words: word[0], word[1], ..., word[n]
+    // create a special word to make the end of the list of words
+    // so tht I print them, when seeing this special word, I know
+    // I must stop
+    word[n+1][0] = EOF;
+    return;
+
+}
+
+int main(){
+    char s[MAXLEN]; // String input
+    char d;         // Delimiter
+
+    // read input from stdinput
+    char c;
+    int i = 0;
+    while(1){
+        c = getchar();
+        if (c == '\n'){
+            // Reach end of first line
+            break;
+        }
+        else{
+            s[i] = c;
+            i++;
+        }
+    }
+    s[i] = 0; // same as '\0'
+
+    // read the delimiter from stdin
+    d = getchar();
+
+    // after I know d and s I am now calling the function
+    char **word; // word[n] is the (n+1)th word in the string
+    splitString(s, d, word);
+
+    // print the words
+    int n = 0;
+    while(1){
+
+        if (word[n][0] == EOF) break;
+
+        printf("%s\n", word[n]);
+        n++;
+        // we need to break out of this loop somehow
+    }
+}
