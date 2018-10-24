105275096878e84b9ffc46fc0c956b632ecfa373
amali24 <apt41790@gmail.com>
2018-10-24 18:51:44 -0400

    Compiles, but doesn't run

--- PointerToPointer.c
+++ PointerToPointer.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char** readLines(int n){
+char** readLines(int *p){
+    int n = *p;
     char** list_of_lines;
     list_of_lines = (char**) malloc(n * sizeof(char*));
     int i;
@@ -11,28 +13,25 @@ char** readLines(int n){
         }
         *(list_of_lines + i) = line;
     }
+    *p = i;
     return list_of_lines;
 }
 
 void printLines(char** list_of_lines, int num_of_lines){
     int i;
     for (i = 0; i < num_of_lines; i ++){
-        printf("%s\n", *(list_of_lines + i))
+        printf("%s\n", *(list_of_lines + i));
     }
 }
 
 int main(int argc, char** argv){
-    
     int n;
-
     if (argc != 2){
         printf("How to run: %s number\n", argv[0]);
         return 0;
     }
-
-    n = argv[1];
-
-    printf("number of arguments = %d\n", argc);
-    
-
+    n = atoi(argv[1]);
+    char** lines = readLines(&n);
+    printLines(lines, n);
+    return 0;    
 }
