7f5c708b93316bc5582534c68b40b08685f8d8c9
amali24 <apt41790@gmail.com>
2018-10-31 18:47:18 -0400

    Added some cool fuctions (still in-class)

--- StructPointer.c
+++ StructPointer.c
@@ -16,6 +16,32 @@ void printList(struct PET *q, int num){
     }
 }
 
+void copyList(struct PET *p_source, struct PET *p_dest, int n){
+    // copy n elements from array point p_source to array pointer p_dest
+    if (p_source == NULL) printf("p_source cannot be NULL");
+
+    // allocate memory to p_dest to represent an array of n elements of stuct PET
+    //class 
+    int i;
+    for (i = 0; i < n; i++){
+        p_dest[i] = p_source[i];
+    }
+    return;
+}
+
+struct PET * copyList2(struct PET *p_source, int n){
+    // copy n elements from array point p_source to array pointer p_dest
+    if (p_source == NULL) printf("p_source cannot be NULL");
+
+    // allocate memory to p_dest to represent an array of n elements of stuct PET
+    struct PET *p_dest = (struct PET *) malloc(n * sizeof(struct PET));
+    int i;
+    for (i = 0; i < n; i++){
+        p_dest[i] = p_source[i];
+    }
+    return p_dest;
+}
+
 int main(){
     // want to have a list of pents (up to n = 2)
 
@@ -35,8 +61,25 @@ int main(){
     (p + 1)->type = "dog"; // or (*(p + 1)).type = "dog"
     (p + 1)->name = "spot"; // or (*(p + 1)). name = "spot"
 
-    // priont this list of pets
-
+    // print this list of pets
+    printf("p is:\n");
     printList(p, 2); // print 2 elemnts in the array pointed two by pointer p
 
+    struct PET *a = (struct PET *) malloc(2 * sizeof(struct PET));
+    copyList(p, a, 2);
+    printf("a is: \n");
+    printList(a, 2);
+
+    struct PET *b;
+    b = copyList2(p, 2);
+    printf("b is:\n");
+    printList(b, 2);
+
+    free(p);
+    p = NULL;
+    free(a);
+    a = NULL;
+    free(b);
+    b = NULL;
+    return 0;
 }
