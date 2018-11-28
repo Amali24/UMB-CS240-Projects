76a3976d987084ea3bba5de3bcb3ff073274bb81
amali24 <apt41790@gmail.com>
2018-11-28 13:12:46 -0500

    HW4 Skeleton Complete

---
+++ hw4.c
@@ -0,0 +1,22 @@
+struct STRING{
+    char *s;
+    struct STRING *next;
+};
+
+void get_strings(struct STRING *list){
+    //read from stdin list of strings and store in linked list given as input
+}
+
+void sort_strings(struct STRING *list){
+    // ascendingly sort strings by length
+}
+
+void show_strings(struct STRING *list){
+    // print contents of list
+}
+
+int main(){
+    struct STRING *list;
+
+    return 0;
+}
