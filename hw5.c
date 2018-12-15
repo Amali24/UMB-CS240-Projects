8825639ffbb6270187122c2dea1eb42b0d4cc11a
amali24 <apt41790@gmail.com>
2018-12-15 12:13:56 -0500

    Input and Ouput working!

--- hw5.c
+++ hw5.c
@@ -21,63 +21,63 @@ struct TreeNode{
 
 struct TreeNode* newNode(char* word, struct Position pos){
     struct TreeNode *new_node = (struct TreeNode*) malloc(sizeof(struct TreeNode));
-    printf("made a node\n");
+    //printf("made a node\n");
     new_node->position = (struct Position*) malloc(1000 * sizeof(struct Position));
-    printf("made a pos\n");
+    //printf("made a pos\n");
     new_node->position->line = pos.line;
     new_node->position->offset = pos.offset;
-    printf("assigned a pos\n");
+    //printf("assigned a pos\n");
     new_node->position->next = NULL;
     new_node->word = (char*) malloc(100 * sizeof(char));
-    printf("made a string\n");
+    //printf("made a string\n");
     strcpy(new_node->word, word);
-    printf("copied string \"%s\" to node\n", word);
+    //printf("copied string \"%s\" to node\n", word);
     new_node->left = new_node->right = NULL;
-    printf("set the branches to null\n\n");
+    //printf("set the branches to null\n\n");
     return new_node;
 }
 
 struct Position* newPosition(int line, int offset){
     struct Position *pos = (struct Position*) malloc(sizeof(struct Position*));
-    printf("Made a new postion\n");
+    //printf("Made a new postion\n");
     pos->line = line;
-    printf("set line to %d\n", line);
+    //printf("set line to %d\n", line);
     pos->offset = offset;
-    printf("set offset to %d\n", offset);
+    //printf("set offset to %d\n", offset);
     pos->next = NULL;
-    printf("set next to null\n\n");
+    //printf("set next to null\n\n");
     return pos;
 }
 
 struct TreeNode *insertNode(struct TreeNode *tree, char *word, struct Position pos){
-    printf("passed word: %s\n", word);
-    printf("passed position: %d, %d\n\n", pos.line, pos.offset);
+    //printf("passed word: %s\n", word);
+    //printf("passed position: %d, %d\n\n", pos.line, pos.offset);
 
     if(tree == NULL){
-        printf("tree was null\n");
+        //printf("tree was null\n");
         struct TreeNode *temp = newNode(word, pos);
-        printf("made new node...\n\n");
+        //printf("made new node...\n\n");
         return temp;
     }
 
     if(strcmp(word, tree->word) < 0){
-        printf("%s < %s, going left\n", word, tree->word);
+        //printf("%s < %s, going left\n", word, tree->word);
         tree->left = insertNode(tree->left, word, pos);
     }
 
     else if(strcmp(word, tree->word) > 0){
-        printf("%s > %s, going right\n", word, tree->word);
+        //printf("%s > %s, going right\n", word, tree->word);
         tree->right = insertNode(tree->right, word, pos);
     }
 
     else if (strcmp(word, tree->word) == 0){
-        printf("word found\n");
+        //printf("word found\n");
         while(tree->position->next != NULL){
             tree->position = tree->position->next;
-            printf("going to next position\n");
+            //printf("going to next position\n");
         }
         tree->position->next = newPosition(pos.line, pos.offset);
-        printf("added position\n\n");
+        //printf("added position\n\n");
     }
 
     return tree;
@@ -109,7 +109,9 @@ void outputAlpha(struct TreeNode *tree){
 }
 
 struct TreeNode* findByPosition(struct TreeNode *tree, int line, int offset){
-    if (tree == NULL) return NULL;
+    if (tree == NULL) {
+        return NULL;
+    }   
     struct Position *pos = tree->position;
     while (pos != NULL){
         if (pos->line == line && pos->offset == offset){
@@ -128,7 +130,7 @@ void output(struct TreeNode *tree){
         while(1){
             struct TreeNode *node = findByPosition(tree, line, offset++);
             if (node == NULL) break;
-            offset ++;
+            //offset ++;
             if (offset >= 2) printf(" ");
             printf("%s", node->word);
         }
@@ -150,9 +152,6 @@ int main(){
 
     int num_cmds = 0;
 
-
-    //TODO: Allocate memory for tree
-
     while (isCommandSection){
         if (fgets(input, MAX_LINE_LENGTH, stdin) == NULL){
             break;
@@ -171,12 +170,12 @@ int main(){
         strcpy(text[num_lines++], input);
     }
 
-    printf("Commands:\n");
+    /*printf("Commands:\n");
 
     for (int i = 0; i < num_cmds; i++){
         printf("%s", commands[i]);
     }
-
+    
     printf("\nText:\n");
 
     for (int i = 0; i < num_lines; i++){
@@ -184,6 +183,7 @@ int main(){
     }
 
     printf("\n\n\n");
+    */
 
 
     struct TreeNode *root = NULL;
@@ -192,17 +192,17 @@ int main(){
 
     for (int i = 0; i < num_lines; i++){
         int j = 0;
-        printf("new for loop (156), i = %d, j = %d\n", i, j);
-        printf("current line is \"%s\"\n", text[i]);
+        //printf("new for loop (156), i = %d, j = %d\n", i, j);
+        //printf("current line is \"%s\"\n", text[i]);
 
         char *token = strtok(text[i], delim);
 
         while(1){
-            printf("Token: %s\n", token );
+            //printf("Token: %s\n", token );
 
             // add token to tree
             struct Position *pos = newPosition(i + 1, j);
-            printf("made a pos with %d, %d\n\n", i + 1, j);
+            //printf("made a pos with %d, %d\n\n", i + 1, j);
             j++;
 
 
@@ -219,7 +219,7 @@ int main(){
 
     // TODO: COMMAND PROCESSING GOES HERE
 
-    outputAlpha(root);
+    //outputAlpha(root);
     output(root);
     printf("\n");
     return 0; 
