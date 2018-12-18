800e9a6593234f6615868eed8c8cf5aed914ecb7
amali24 <apt41790@gmail.com>
2018-12-17 19:24:35 -0500

    Done.

--- final1.c
+++ final1.c
@@ -8,19 +8,25 @@ int main (){
     char* delim = (char*) malloc(sizeof(char) * MAX_LINE_LEN);
     char* text = (char*) malloc(sizeof(char) * MAX_LINE_LEN);
 
-    int j = 0;
+    delim = fgets(delim, MAX_LINE_LEN, stdin);
+    text = fgets(text, MAX_LINE_LEN, stdin);
 
-    fgets(delim, MAX_LINE_LEN, stdin);
-    fgets(text, MAX_LINE_LEN, stdin);
+    if (delim == NULL || text == NULL){
+        return 1;
+    }
 
 
     char *token = strtok(text, delim);
 
+    int i = 1; 
     while(1){
-    
+        printf("%s,%d", token, i++);
+
         token = strtok(NULL, delim);
 
         if(token == NULL) break;
+
+        printf("\n");
 
     }
     return 0;
