40912d2191ecc77a589e8832f7053896c16c9949
amali24 <apt41790@gmail.com>
2018-12-17 19:16:25 -0500

    put in skeleton from hw5

--- final1.c
+++ final1.c
@@ -3,5 +3,25 @@
 #include <string.h>
 
 int main (){
+    const int MAX_LINE_LEN = 1000;
+
+    char* delim = (char*) malloc(sizeof(char) * MAX_LINE_LEN);
+    char* text = (char*) malloc(sizeof(char) * MAX_LINE_LEN);
+
+    int j = 0;
+
+    fgets(delim, MAX_LINE_LEN, stdin);
+    fgets(text, MAX_LINE_LEN, stdin);
+
+
+    char *token = strtok(text, delim);
+
+    while(1){
     
+        token = strtok(NULL, delim);
+
+        if(token == NULL) break;
+
+    }
+    return 0;
 }
